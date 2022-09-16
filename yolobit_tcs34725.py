267add534d11ad2067ef7fe85ec687b0f0544568
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-09-16 22:47:03 +0700

    Update yolobit_tcs34725.py

--- yolobit_tcs34725.py
+++ yolobit_tcs34725.py
@@ -45,7 +45,7 @@ COLOR = {
 
 class TCS34725:
     def __init__(self, i2c, address=0x29):
-        self.status = 0
+        status = 0
         self.i2c = i2c
         self.address = address
         self._active = False
@@ -54,9 +54,9 @@ class TCS34725:
         if sensor_id not in (0x44, 0x10):
             #raise RuntimeError
             print("wrong sensor id 0x{:x}".format(sensor_id))
-            self.status = 0
+            status = 0
         else:
-            self.status = 1
+            status = 1
 
 
     def _register8(self, register, value=None):
@@ -201,8 +201,10 @@ class ColorSensor:
         self.address = address
         scl_pin = machine.Pin(22)
         sda_pin = machine.Pin(21)
+        status = 0 
         try:
             self.tcs = TCS34725(machine.SoftI2C(scl=scl_pin, sda=sda_pin), self.address)
+            status = 1 
         except:
             print('Color sensor not found')
             #raise Exception('Color sensor not found')
@@ -213,7 +215,7 @@ class ColorSensor:
         color_sensor.read(0, 'r')
         range of value return: 0 - 255 (type int)
         '''
-        if self.status == 1 :
+        if status == 1 :
             return self.tcs.html_rgb()[COLOR[color]]
         else:
             print('0')
@@ -229,7 +231,7 @@ class ColorSensor:
                 white(16, 16, 16)
                 yellow (30, 15, 4)
         '''
-        if self.status ==1:
+        if status ==1:
             r, g, b = self.tcs.html_rgb()
             if max(r, g, b, limit) == r:
             #red
